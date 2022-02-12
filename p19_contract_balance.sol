2d82b5b977eaf8259961c78a8c0751379488aa7f
Neeraj Singh <92664496+neerajofficial@users.noreply.github.com>
2022-02-13 00:06:52 +0530

    Update p19_contract_balance.sol

--- p19_contract_balance.sol
+++ p19_contract_balance.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.0;
 
 contract Pay {
     //  Contract Balance
+    address payable user = payable(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+
     function payEther() public payable {
-        
     }
+
     function getBalance() public view returns(uint) {
         return address(this).balance;
+    }
+    function sendEtherToAccount() public {
+        user.transfer(1 ether);
     }
 }
